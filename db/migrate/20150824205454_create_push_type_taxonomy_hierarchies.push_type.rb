81b4b974c2ff46340fc58b66147d8a4a212ad203
Jordan Elver <jordan.elver@gmail.com>
2015-08-24 22:16:54 +0100

    Install PushType

---
+++ db/migrate/20150824205454_create_push_type_taxonomy_hierarchies.push_type.rb
@@ -0,0 +1,17 @@
+# This migration comes from push_type (originally 20150310123230)
+class CreatePushTypeTaxonomyHierarchies < ActiveRecord::Migration
+  def change
+    create_table :push_type_taxonomy_hierarchies, id: false do |t|
+      t.uuid      :ancestor_id,     null: false
+      t.uuid      :descendant_id,   null: false
+      t.integer   :generations,     null: false
+    end
+
+    add_index :push_type_taxonomy_hierarchies, [:ancestor_id, :descendant_id, :generations],
+      unique: true,
+      name: "taxonomy_anc_desc_idx"
+
+    add_index :push_type_taxonomy_hierarchies, [:descendant_id],
+      name: "taxonomy_desc_idx"
+  end
+end
